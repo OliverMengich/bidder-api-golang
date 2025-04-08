55c2ad64df8af1bfc9f05c9d8958d253ee6463bf
OliverMengich <kemeioliver2000@gmail.com>
2025-04-08 15:03:09 +0300

    feat: sets collections for props

--- src/db/db.go
+++ src/db/db.go
@@ -7,16 +7,28 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
-
-var collection *mongo.Collection
+var (
+    Client     *mongo.Client
+    DB         *mongo.Database
+    AuctionsCol   *mongo.Collection
+    ProductsCol *mongo.Collection
+    BiddersCol   *mongo.Collection
+    BidsCol *mongo.Collection
+)
 
 func ConnectToMongo() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/go-mongo-api")
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err !=nil{
 		log.Fatal(err)
 		return nil, err
 	}
 	log.Println("Connnected to MongoDB")
+	Client = client
+	DB = client.Database("auction-db") 
+	AuctionsCol = DB.Collection("auctions")
+	ProductsCol = DB.Collection("products")
+	BiddersCol = DB.Collection("bidders")
+	BidsCol = DB.Collection("bids")
 	return client, nil
 }
