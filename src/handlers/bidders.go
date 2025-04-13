1e86074a5e2b926e3fe854eefeb89d8f9391fc5b
OliverMengich <kemeioliver2000@gmail.com>
2025-04-13 21:39:59 +0300

    chore: registering bidder

--- src/handlers/bidders.go
+++ src/handlers/bidders.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/OliverMengich/bidder-api-golang/src/services"
@@ -9,5 +11,20 @@ import (
 var bidder services.Bidder
 
 func registerBidder(w http.ResponseWriter, r *http.Request) {
+	err := json.NewDecoder(r.Body).Decode(&bidder)
+	if err != nil {
+		log.Println(err)
+		responseWithError(w, 400, "Error adding Product")
+	}
 
+	err = bidder.RegisterBidder(bidder)
+	if err != nil {
+		responseWithError(w, 400, "Error registering Bidder ")
+		return
+	}
+	res := Response{
+		message: "Successfully Registered new member",
+		code:    201,
+	}
+	respondWithJSON(w, res.code, res)
 }
