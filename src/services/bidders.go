fd61eec3cd735d91214b79e4a440eea9f4123b68
OliverMengich <kemeioliver2000@gmail.com>
2025-04-14 20:58:35 +0300

    fix: collection

--- src/services/bidders.go
+++ src/services/bidders.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/OliverMengich/bidder-api-golang/src/db"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -12,15 +13,15 @@ import (
 type Bidder struct {
 	ID           string               `json:"id,omitempty" bson:"_id,omitempty"`
 	BidderNumber float64              `json:"bidder_number,omitempty" bson:"bidder_number,omitempty"`
-	Name         string               `json:"name,omitempty" bson:"amount,omitempty"`
+	Name         string               `json:"name,omitempty" bson:"name,omitempty"`
 	CreatedAt    time.Time            `json:"created_at,omitempty" bson:"created_at,omitempty"`
 	Updated      time.Time            `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
-	Auctions     []primitive.ObjectID `json:"auctions,omitempty" bson:"auctions,omitempty"`
+	Auctions     []primitive.ObjectID `json:"auctions" bson:"auctions"`
 }
 
 func (b *Bidder) GetAllBidders() ([]Bidder, error) {
-	collection := returnCollectionPointer("auctions")
-	var bidders []Bidder
+	collection := db.BiddersCol
+	var bidders []Bidder = []Bidder{}
 	cursor, err := collection.Find(context.TODO(), bson.D{})
 	if err != nil {
 		log.Fatal(err)
@@ -36,12 +37,13 @@ func (b *Bidder) GetAllBidders() ([]Bidder, error) {
 }
 
 func (b *Bidder) RegisterBidder(bidder Bidder) error {
-	collection := returnCollectionPointer("bidders")
+	collection := db.BiddersCol
 	_, err := collection.InsertOne(context.TODO(), Bidder{
 		Name:         bidder.Name,
 		CreatedAt:    time.Now(),
 		BidderNumber: bidder.BidderNumber,
 		Updated:      time.Now(),
+		Auctions:     []primitive.ObjectID{},
 	})
 	if err != nil {
 		log.Println("Error: ", err)
